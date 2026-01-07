5989395330643152eb5e435fb4d17fdb2515007f
fasna <fasna2003@gmail.com>
2026-01-07 15:16:36 +0530

    Animated avatar with Mixamo animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 
+
+
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
       <color attach="background" args={["#ececec"]} />
+      {/* <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} intensity={1} /> */}
+      
       <Experience />
+      
     </Canvas>
   );
 }
